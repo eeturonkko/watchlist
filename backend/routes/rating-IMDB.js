517ed16a936058343f4291ef176856631dddc46f
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2025-05-06 10:45:17 +0300

    Refactor route definition

--- backend/routes/rating-IMDB.js
+++ backend/routes/rating-IMDB.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const fetch = require('node-fetch');
+const express = require("express");
+const fetch = require("node-fetch");
 const router = express.Router();
 
 const OMDB_API_KEY = process.env.OMDB_API_KEY;
 
-router.get('/movie-rating/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const imdbId = req.params.id;
 
   try {
     // Haetaan elokuvan arvosana OMDB API:sta
-    const response = await fetch(`http://www.omdbapi.com/?i=${imdbId}&apikey=${OMDB_API_KEY}`);
+    const response = await fetch(
+      `http://www.omdbapi.com/?i=${imdbId}&apikey=${OMDB_API_KEY}`
+    );
     const data = await response.json();
 
     // logita Apin vastaus konsoliin koska ei se nyt toiminut :SS
     console.log("OMDB API Response:", data);
 
-    if (data.Response === 'True') {
+    if (data.Response === "True") {
       // onnistunut vastaus palauttaaa IMDb-arvosanan
       res.json({ imdbRating: data.imdbRating });
     } else {
@@ -24,8 +26,8 @@ router.get('/movie-rating/:id', async (req, res) => {
     }
   } catch (err) {
     // Logita virhe ja palauta virheviesti
-    console.error('Error fetching IMDb rating:', err);
-    res.status(500).json({ error: 'Failed to fetch IMDb rating from OMDB' });
+    console.error("Error fetching IMDb rating:", err);
+    res.status(500).json({ error: "Failed to fetch IMDb rating from OMDB" });
   }
 });
 
